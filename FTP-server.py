c005354a16b6fe4a9cb888e53060fded2c0ab12c
Joshua Fletcher <fletchjosh246@gmail.com>
2022-04-12 21:28:36 -0400

    Update FTP-server.py

--- FTP-server.py
+++ FTP-server.py
@@ -14,35 +14,33 @@ server_socket.listen()
 def handle_request(connection_socket):
     print("connected")
     request = "placeholder"
-    while request.upper() != "QUIT":
 
-        request = connection_socket.recv(buffer_size).decode('utf-8')
-        print(request)
+    request = connection_socket.recv(buffer_size).decode('utf-8')
+    print(request)
 
-        if "GET" in request.upper():
-            split_request = request.split()
-            requestType = split_request[0]
-            fileName = split_request[1]
+    if "GET" in request.upper():
+        split_request = request.split()
+        requestType = split_request[0]
+        fileName = split_request[1]
+        requestIP = split_request[2]
+        requestPort = split_request[3]
 
-            data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            data_socket.connect((requestIP, int(requestPort)))
+        confirmation = "Successfully Downloaded " + fileName
 
-            try:
-                open_file = open(fileName,'rb')
-                requested_file = open_file.read()
-                open_file.close()
-                data_socket.send(requested_file)
-            except FileNotFoundError:
-                error_message = "File Not Found on Server"
-                data_socket.send(error_message.encode('utf-8'))
+        data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        data_socket.connect((requestIP, int(requestPort)))
+
+        try:
+            open_file = open(fileName,'rb')
+            requested_file = open_file.read()
+            open_file.close()
+            data_socket.send(requested_file)
+        except FileNotFoundError:
+            error_message = "File Not Found on Server"
+            data_socket.send(error_message.encode('utf-8'))
             
-            data_socket.close()
+        data_socket.close()
 
-        elif "CONNECT" in request.upper():
-            split_request = request.split()
-            requestType = split_request[0]
-            requestIP = split_request[1]
-            requestPort = split_request[2]
 
     connection_socket.close()
 
